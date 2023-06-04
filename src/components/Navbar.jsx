e0317587ee0a0ea2355a27f6f0749ea56e8b8bd3
Salahadin Dinsafa <salahadindinsafa@gmail.com>
2023-06-04 15:03:09 +0300

    navbar partial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,21 @@
-
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/logo.webp';
+import { BsThreeDotsVertical, BsPersonCircle } from 'react-icons/bs';
 
-
+import logo from '../assets/images/logo.PNG';
 
 
 function Navbar() {
 
     return (
-        <div className='shadow-sm w-full static left-0'>
-            <div className='md:flex md:px-10 pr-5 md:justify-between'>
-                <div className='cursor-pointer items-center'>
-                    <Link to='' >
-                        <img src={logo} alt="LogoImage" className="rounded-full h-10 md:h-16" />
-                    </Link>
-
-
-                </div>
-
+        <div className='shadow-sm w-full static mb-2 flex justify-between px-10 pt-2'>
+            <div className='cursor-pointer items-center w-fit h-fit'>
+                <Link to='' >
+                    <img className='h-14 w-14 object-scale-down rounded-full ' src={logo} alt="LogoImage" />
+                </Link>
+            </div>
+            <div className='w-16 flex justify-between items-center text-gold'>
+                <BsPersonCircle onClick={() => console.log('todo')} className='text-3xl cursor-pointer rounded-full p-1 hover:bg-slate-300' />
+                <BsThreeDotsVertical onClick={() => console.log('todo')} className='text-2xl cursor-pointer rounded-full p-1 hover:bg-slate-300' />
             </div>
         </div >
     )
